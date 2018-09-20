ae5c5e6cd98ca63813302167284d8891040f53c1
Damian Wasilewicz <dwasilewicz@stuy.edu>
2018-09-19 23:35:16 -0400

    And comments

--- 08_lemme_flask_u_sumpn/RoutingFlask.py
+++ 08_lemme_flask_u_sumpn/RoutingFlask.py
@@ -2,18 +2,24 @@
 #SoftDev1 Pd6
 #K #08: Fill Yer Flask
 #2018-09-19
+
+#Imports flask from Flask, and render template in order to load html templates
 from flask import Flask, render_template
 app = Flask(__name__)
 
+#route for main page
 @app.route("/")
 def main_page():
+    #loads html template
     return render_template("base.html")
 
 
-
+#route for info, found by adding more to end of url
 @app.route("/more")
 def basic_info():
     return render_template("info.html")
+
+#route for hobbies, found by adding hobbies to end of url
 
 @app.route("/hobbies")
 def hobbies():
