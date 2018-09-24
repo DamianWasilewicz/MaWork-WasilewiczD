c46c113cf2f8c4ff375fae407b77429bc7741cac
Damian Wasilewicz <dwasilewicz@stuy.edu>
2018-09-23 21:39:12 -0400

    Got assignment working, working on links

--- 10_occupy_flask_st/ErlenmeyerFlasks.py
+++ 10_occupy_flask_st/ErlenmeyerFlasks.py
@@ -1,38 +1,61 @@
 from flask import Flask, render_template
 import csv, random
+from csv import reader
 app = Flask(__name__)
 
 
 #creates dictionary (empty)
 OCCLIST = {}
+LINKLIST = {}
 #takes in file, returns random occupation with weighted probability
 def randomocc(filename):
     #opens file and reads it
     file = open(filename, "r")
-    red = file.read()
-    #split by lines, excluding title and empty line at bottom
-    lines = red.split("\n")[1:-1]
-    read = csv.reader(lines)
-    #iterates, adds key and weight to OCCLIST
-    for r in read:
-        #creates list
-        #mylist=list()
-        #mylist.append(float(r[1]))
-        #mylist.append(r[2])
-        OCCLIST[r[0]]=mylist
+    raw = reader(file)
+    next(file)
+    for r in raw:
+        OCCLIST[r[0]] = float(r[1])
     #to be polite
+    del OCCLIST['Total']
     file.close()
     return OCCLIST
+def linkify(filename):
+    #opens file and reads it
+    file = open(filename, "r")
+    raw = reader(file)
+    next(file)
+    #iterates, adds key and weight to OCCLIST
+    for r in raw:
+        LINKLIST[r[0]] = str(r[2])
+    #to be polite
+    del LINKLIST['Total']
+    file.close()
+    return LINKLIST
 #route for main page
 @app.route("/")
 def main_page():
     #loads html template
     return render_template("home.html")
 
+def randomO(table):
+    #random number
+    randy = random.uniform(0, 99.8)
+    #counter keeps track of what percentage we're up to
+    count = 0
+    #returns key based with weighted probability
+    for key, value in OCCLIST.items():
+        #compares random number to % we're up to;
+        #if current percentage is greater than randy, return key
+        if count + value >= randy:
+            return key
+        #if not, add counter to current percentage
+        count += value
 @app.route("/occupations")
 def occupy():
     return render_template("occupations.html",
-    table = randomocc("occupations.csv"))
+    table = randomocc("occupations.csv"),
+    rand = randomO(randomocc("occupations.csv")),
+    lnks = linkify("occupations.csv"))
 
 if __name__ == "__main__":
     app.debug = True
