bb172f6e9e08d78bbca54a7cc560fc946078ecfc
Damian Wasilewicz <dwasilewicz@stuy.edu>
2018-10-07 09:01:17 -0400

    Got avg computer working, getting grades

--- 17_db-nmcrnch/HollyHalls_XiaS-WasilewiczD.py
+++ 17_db-nmcrnch/HollyHalls_XiaS-WasilewiczD.py
@@ -14,48 +14,62 @@ DB_FILE = "discobandit.db"
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops
 
-
 comm_peeps = """
-CREATE TABLE if not exists mypeeps(
+CREATE TABLE IF NOT EXISTS mypeeps(
     id INTEGER PRIMARY KEY,
     name TEXT,
-    age INTEGER)
+    age INTEGER,
+    osis INTEGER)
 """
 comm_courses = """
-CREATE TABLE if not exists classes(
+CREATE TABLE IF NOT EXISTS classes(
+    id INTEGER PRIMARY KEY,
     name TEXT,
     mark INTEGER,
-    pd INTEGER)
+    osis INTEGER)
 """
 #execute command lines creating tables
 c.execute(comm_peeps)
 c.execute(comm_courses)
 
 #populating table - peeps
-with open("templates/peeps.csv") as file:
+with open('templates/peeps.csv', 'r') as file:
     read = csv.DictReader(file)
     for r in read:
         comm_peeps = "INSERT INTO mypeeps(name, age, id) VALUES('" + r['name'] + "', " + r['age'] + ", " + r['id'] + ");"
         c.execute(comm_peeps)
 
 #populating table-comm_occupations
-with open("templates/courses.csv") as file:
+with open('templates/courses.csv', 'r') as file:
     read = csv.DictReader(file)
     for r in read:
-        comm_courses = "INSERT INTO classes(name, mark, pd) VALUES('" + r['code'] + "', " + r['mark'] + ", " + r['id'] + ");"
+        comm_courses = "INSERT INTO classes(name, mark, osis) VALUES('"+ r['code'] + "', " + r['mark'] + ", " + r['id'] + ");"
         c.execute(comm_courses)
-
-def stalk(studentname): """
+comm_grade = """
     SELECT
-        name,
-        age,
-        id
+        mark
     FROM
-        peeps
+        classes
     WHERE
-        name = studentname
+        osis == 3
     """
+def computeAvg():
+    list = c.execute(comm_grade)
+    number = list.fetchone()[0]
+    number2 = list.fetchone()[0]
+    print((number + number2)/ 2)
 
+comm_avg = """
+    CREATE TABLE IF NOT EXISTS avgs(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        osis INTEGER,
+        avg INTEGER)
+        """
+c.execute(comm_avg)
+
+#comm_avgs = "INSERT INTO avgs(name, osis, avg) VALUES('"+ 'sasha' + "', " + '3' + ", " + 'int(computeAvg())' + ");"
+#c.execute(comm_avgs)
 db.commit() #save changes
-print "Sucess"
+computeAvg()
 db.close()  #close database
