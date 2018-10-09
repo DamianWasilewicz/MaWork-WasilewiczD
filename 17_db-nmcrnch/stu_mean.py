8a4fa91342f5c1528c95ab31d18ac7ae0fbb017f
Damian Wasilewicz <dwasilewicz@stuy.edu>
2018-10-08 21:24:08 -0400

    Finished working version

--- 17_db-nmcrnch/stu_mean.py
+++ 17_db-nmcrnch/stu_mean.py
@@ -1,3 +1,9 @@
+#Holly Halls
+#Damian Wasilewicz, Sophia Xia
+#SoftDev1 pd1
+#K17 -- Average, ... or Basic?
+#2018-10-07
+
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitates CSV I/O
 import os        #for deleting discobandit
@@ -33,14 +39,14 @@ c.execute(comm_peeps)
 c.execute(comm_courses)
 
 #populating table - peeps
-with open('templates/peeps.csv', 'r') as file:
+with open('data/peeps.csv', 'r') as file:
     read = csv.DictReader(file)
     for r in read:
         comm_peeps = "INSERT INTO mypeeps(name, age, id) VALUES('" + r['name'] + "', " + r['age'] + ", " + r['id'] + ");"
         c.execute(comm_peeps)
 
 #populating table-comm_occupations
-with open('templates/courses.csv', 'r') as file:
+with open('data/courses.csv', 'r') as file:
     read = csv.DictReader(file)
     for r in read:
         comm_courses = "INSERT INTO classes(name, mark, osis) VALUES('"+ r['code'] + "', " + r['mark'] + ", " + r['id'] + ");"
@@ -48,32 +54,37 @@ with open('templates/courses.csv', 'r') as file:
 
 #============================================================================
 
-
+osis_num = 1;
 
 #get grades according to osis number
-def get_grade(osis_num, counter):
+def get_grade(osis_num):
     comm_grade = "SELECT mark FROM classes WHERE osis ="
     comm_grade += str(osis_num)
-    c.execute(comm_grade)
-    a = c.fetchall()
-    answer = 0
-    while counter >= 0:
-        answer = answer + int((a[counter][0]))
-        counter = counter - 1
-    return answer
+    return c.execute(comm_grade).fetchall()
+print get_grade(osis_num)
+
 #returns how many grades a student has
 def get_length(osis_num):
     comm_length = "SELECT count(*) FROM classes WHERE osis ="
     comm_length += str(osis_num)
     return c.execute(comm_length).fetchone()[0]
-#returns avg grade of a student given his id
-def get_avg(osis_num):
-    return int(get_grade(osis_num, get_length(osis_num) - 1)) / int(get_length(osis_num))
+print get_length(osis_num)
 
+#gets average according to osis number
+def computeAvg(osis_num):
+    length = get_length(osis_num)
+    ctr = length -1
+    cur = get_grade(osis_num)
+    retVal = 0
+    while ctr >= 0:
+        retVal += int(cur[ctr][0])
+        ctr -= 1
+    return int(float(retVal/length))
+print computeAvg(osis_num)
 
 #Create table for averages
 comm_avg = """
-    CREATE TABLE IF NOT EXISTS avgs(
+    CREATE TABLE IF NOT EXISTS peeps_avgs(
         id INTEGER PRIMARY KEY,
         name TEXT,
         avg INTEGER,
@@ -81,27 +92,32 @@ comm_avg = """
         """
 c.execute(comm_avg)
 
-#_________________Everything up to here works I think
 #Populate avg table
-osis_num = 1
-#while osis_num < 11:
-#    comm_getname = "SELECT name FROM mypeeps WHERE osis = "
-#    comm_getname += str(osis_num)
-    #c.execute(comm_getname)
-    #name = c.fetchone()
-    #avg = get_avg(osis_num)
-    #comm_peeps = "INSERT INTO mypeeps(name, id, avg) VALUES('" + 'Sasha' + "', " + osis_num + ", " + avg + ");"
-    #c.execute(comm_peeps)
-    #osis_num += 1
-
+while osis_num < 11:
+    comm_getname = "SELECT name FROM mypeeps WHERE mypeeps.id = "
+    comm_getname += str(osis_num)
+    name = c.execute(comm_getname).fetchone()[0]
+    #print name
+    avg = computeAvg(osis_num)
+    #print avg
+    comm_pop = "INSERT INTO peeps_avgs(name, avg, osis) VALUES(\"" + name + "\", " + str(avg) + ", " + str(osis_num) + ");"
+    c.execute(comm_pop)
+    osis_num += 1
 
 #display avg table
-comm_display_avg= "SELECT *FROM peeps_avg"
+comm_display_avg= "SELECT *FROM peeps_avgs"
+display = c.execute(comm_display_avg)
+ctr = 0
+while ctr < 10:
+    print display.fetchone()
+    ctr += 1
 
+#add a course to the table classes
+def add_course(nname, nmark, nosis):
+    comm_add_course = "INSERT INTO classes(name, mark, osis) VALUES(\"" + nname + "\", " + str(nmark) + ", " + str(nosis) + ");"
+    c.execute(comm_add_course)
 
-#comm_avgs = "INSERT INTO avgs(name, osis, avg) VALUES('"+ 'sasha' + "', " + '3' + ", " + 'int(computeAvg())' + ");"
-#c.execute(comm_avgs)
+add_course("IT", 100, 11)
 
 db.commit() #save changes
-print(c.fetchall())
 db.close()  #close database
