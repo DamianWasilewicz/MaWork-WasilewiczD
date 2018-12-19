0a52579d0008003f597db2597467fccc868c6da8
Damian Wasilewicz <dwasilewicz@stuy.edu>
2018-12-18 20:09:45 -0500

    Added comments

--- 28_js/funky_town.js
+++ 28_js/funky_town.js
@@ -3,6 +3,11 @@
 // K#28 -- Sequential Progression
 // 2018-12-18
 
+/**
+  * Function that returns the nth value in the fibonacci sequence
+  * @param {num} index of value in fib sequence
+  * @return {num} nth value in fib sequence
+  */
 
 var fibb = (num) => {
   if (num == 0) {
@@ -12,7 +17,13 @@ var fibb = (num) => {
       return (fibb(num - 1)*num)
     }
 }
-
+/**
+  * Function that makes use of Euclid's algorithm to find the greatest
+  * common denominator of two integers
+  * @params {num} a - first number
+  * @params {num} b - second number
+  * @return {num} greatest common denominator of a and b
+  */
 var gcd = (a, b) => {
   if (a < b){
     var c = b
@@ -28,9 +39,14 @@ var gcd = (a, b) => {
   }
 }
 
-
+/**
+  * Helper, list of names
+  */
 var list = ["Damian W.", "Sajed N.", "Tim M.", "Hello", "Jello", "Bello"]
-
+/**
+  * Function returns random student name from list of names
+  * @return Random Student Name
+  */
 var randomStudent = () => {
   var number = Math.ceil(Math.random() * (list.length)) - 1
   return list[number]
